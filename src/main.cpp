3953b2b6f6f015487ddfde35db5547ca37f4d98c
PHRupp <pat.h.rupp@gmail.com>
2023-12-10 14:10:11 -0600

    Working version with access to subtypes within object

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,30 @@
 
 namespace py = pybind11;
 
-int add(int i, int j) {
-    return i + j;
+class Data {
+public:
+	Data(double a){this->b=a;}
+	double b;
+};
+
+class Msg {
+public:
+	Msg(int f){this->d = Data(f*3.0);}
+	Data d = Data(0);
+};
+
+Msg add(int i, int j) {
+    return Msg( (double)i+j );
 }
 
-PYBIND11_MODULE(cmake_example, m) {
+PYBIND11_MODULE(cwrap, m) {
     m.doc() = "";
     m.def("add", &add);
+	py::class_<Msg>(m, "Msg")
+		.def(py::init<int>(), "Msg", py::arg("f"))
+		.def_readwrite("d", &Msg::d)
+	;
+	py::class_<Data>(m, "Data")
+		.def_readwrite("b", &Data::b)
+	;
 }
